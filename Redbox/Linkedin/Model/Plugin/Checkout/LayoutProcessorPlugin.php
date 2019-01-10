f410c24837abc63b909b2f3a543371b55c41d342
Kos Zima <kos.zyma@gmail.com>
2019-01-11 00:09:44 +0200

    [RedBox] Task

--- Redbox/Linkedin/Model/Plugin/Checkout/LayoutProcessorPlugin.php
+++ Redbox/Linkedin/Model/Plugin/Checkout/LayoutProcessorPlugin.php
@@ -9,6 +9,22 @@ namespace Redbox\Linkedin\Model\Plugin\Checkout;
 class LayoutProcessorPlugin
 {
     /**
+     * Customer session
+     *
+     * @var \Magento\Customer\Model\Session
+     */
+    private $customerSession;
+
+    /**
+     * @param \Magento\Customer\Model\Session $customerSession
+     */
+    public function __construct(
+        \Magento\Customer\Model\Session $customerSession
+    ) {
+        $this->customerSession = $customerSession;
+    }
+
+    /**
      * @param \Magento\Checkout\Block\Checkout\LayoutProcessor $subject
      * @param array $jsLayout
      * @return array
@@ -17,34 +33,36 @@ class LayoutProcessorPlugin
         \Magento\Checkout\Block\Checkout\LayoutProcessor $subject,
         array $jsLayout
     ) {
+        if (!$this->customerSession->isLoggedIn()) {
 
-        $customAttributeCode = 'linkedin_profile';
-        $customField = [
-            'component' => 'Magento_Ui/js/form/element/abstract',
-            'config' => [
-                'customScope' => 'shippingAddress.custom_attributes',
-                'customEntry' => null,
-                'template' => 'ui/form/field',
-                'elementTmpl' => 'ui/form/element/input',
-                'tooltip' => [
-                    'description' => 'this is what the field is for',
+            $customAttributeCode = 'linkedin_profile';
+            $customField = [
+                'component' => 'Magento_Ui/js/form/element/abstract',
+                'config' => [
+                    'customScope' => 'shippingAddress.custom_attributes',
+                    'customEntry' => null,
+                    'template' => 'ui/form/field',
+                    'elementTmpl' => 'ui/form/element/input',
+                    'tooltip' => [
+                        'description' => 'this is what the field is for',
+                    ],
                 ],
-            ],
-            'dataScope' => 'shippingAddress.custom_attributes' . '.' . $customAttributeCode,
-            'label' => 'Linkedin Profile',
-            'provider' => 'checkoutProvider',
-            'sortOrder' => 0,
-            'validation' => [
-                'required-entry' => true
-            ],
-            'options' => [],
-            'filterBy' => null,
-            'customEntry' => null,
-            'visible' => true,
-        ];
+                'dataScope' => 'shippingAddress.custom_attributes' . '.' . $customAttributeCode,
+                'label' => 'Linkedin Profile',
+                'provider' => 'checkoutProvider',
+                'sortOrder' => 0,
+                'validation' => [
+                    'required-entry' => true
+                ],
+                'options' => [],
+                'filterBy' => null,
+                'customEntry' => null,
+                'visible' => true,
+            ];
 
-        $jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']['children']
-        ['shippingAddress']['children']['shipping-address-fieldset']['children'][$customAttributeCode] = $customField;
+            $jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']['children']
+            ['shippingAddress']['children']['shipping-address-fieldset']['children'][$customAttributeCode] = $customField;
+        }
 
         return $jsLayout;
     }
---
+++ Redbox/Linkedin/README.md
@@ -0,0 +1,1 @@
+<h2>Redbox_Linkedin Demo Extension</h2>
